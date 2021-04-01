126dc280d5c11c0c34c476b037f7103852e77bb6
annastrelniece <78434097+annastrelniece@users.noreply.github.com>
2021-04-01 20:33:05 +0300

    Update main.js

---
+++ task-2/js/main.js
@@ -0,0 +1,12 @@
+// 2. Write a JavaScript function to get the values of First and Last name of the following form.
+//Sample HTML file :
+
+function getFormvalue(submitEvent) {
+    submitEvent.preventDefault();
+    const FirstNameElement =  document.getElementById('FirstName');
+    const LastNameElement =  document.getElementById('LastName');
+    const FirstName = FirstNameInputElement.value;
+    const LastName = LastNameInputElement.value;
+}
+    console.log (FirstName);
+    console.log (LastName);
