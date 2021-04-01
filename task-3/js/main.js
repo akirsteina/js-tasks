8cd3e9140d76789619047c492edb064bf648d154
akirsteina <akirsteina@users.noreply.github.com>
2021-04-01 20:46:50 +0300

    add task 3

---
+++ task-3/js/main.js
@@ -0,0 +1,7 @@
+// Write a JavaScript program to set the 
+// background color of a paragraph.
+
+function setBackgroundOfParagraph() {
+    const pElement = document.getElementById('blue-paragraph');
+    pElement.style.backgroundColor = 'blue';
+}
